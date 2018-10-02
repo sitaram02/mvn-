829d5a37dda99a90de78adc859079561cc7d48b1
root <root@ip-172-31-41-19.us-east-2.compute.internal>
2018-10-02 23:10:11 +0000

    adding simple_java

---
+++ simple_java/src/main/java/imc/formac/Ram/App.java
@@ -0,0 +1,13 @@
+package imc.formac.Ram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
